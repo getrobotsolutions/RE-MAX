5daf6fe4ad6698dac61a8c5412dc46a12dff8ba0
tamoorkayani <tamoorkayani@gmail.com>
2019-10-29 23:55:16 +0500

    Update
    Update

--- Contents/Selfie/Scripts/PhotoPrinting.js
+++ Contents/Selfie/Scripts/PhotoPrinting.js
@@ -226,7 +226,7 @@ function SendEmail()
     var tempEmail=message;
     //var addr = "paul@robotaisolutions.com";
     var subject = "Your robot selfie";
-    var body = "Thank you for attending the NAMR DAS All staff offsite 2019! Here is your photo from our robot.";
+    var body = "Thank you for visiting the RE/MAX Showcase booth! Here is your photo from our robot.";
     //var mail_server = "smtp.gmail.com";
     //var mail_addr_sender = "ict.muscatairport@gmail.com";
     //var pswd = "123456789K";
